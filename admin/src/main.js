0291c039f2dcfb2e064702e2c0974894d2c63a13
xiehaopei <15907678645@163.com>
2020-11-30 23:44:13 +0800

    install element3

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'element3/lib/theme-chalk/index.css'
+import Element3 from 'element3'
 
-createApp(App).mount('#app')
+createApp(App).use(Element3).mount('#app')
