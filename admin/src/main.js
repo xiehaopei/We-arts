e5153316aeea8b04378b2091b2a87aac2c3c9b96
xiehaopei <15907678645@163.com>
2020-12-03 11:49:00 +0800

    init

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,6 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'element3/lib/theme-chalk/index.css'
-import Element3 from 'element3'
 
-createApp(App).use(Element3).mount('#app')
+createApp(App).mount('#app')
