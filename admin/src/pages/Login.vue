b3322ac33eff3d17a2aed2c0511ff505abffc37f
xiehaopei <15907678645@163.com>
2020-12-05 16:45:53 +0800

    feat:完善login页面CSS细节

--- admin/src/pages/Login.vue
+++ admin/src/pages/Login.vue
@@ -1,27 +1,65 @@
 <template>
   <div class="container">
     <el-card class="login-box">
-      <img src="../assets/login-0.png" class="bear" />
-      <img src="../assets/login-1.png" class="bear hidden" />
-      <img src="../assets/login-2.png" class="bear hidden" />
+      <img src="../assets/login-0.png" class="bear" ref="img0" />
+      <img src="../assets/login-1.png" class="bear-focus hidden" ref="img1" />
+      <img src="../assets/login-2.png" class="bear-focus hidden" ref="img2" />
       <h1 class="title">Login</h1>
-      <el-input id="user" v-model="user" placeholder="请输入用户名"></el-input>
-      <el-input id="password" v-model="password" placeholder="请输入密码" show-password></el-input>
-      <el-button type="primary" class="login-in">Login in</el-button>
+      <el-input id="user" v-model="user" placeholder="请输入用户名" @focus="focus" @blur="blur"></el-input>
+      <el-input
+        id="password"
+        v-model="password"
+        placeholder="请输入密码"
+        show-password
+        @focus="focus"
+        @blur="blur"
+        @keydown="login"
+      ></el-input>
+      <el-button type="primary" class="login-in" @click="login">Login in</el-button>
     </el-card>
   </div>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue';
+import { ref } from 'vue';
+import router from '../router.js';
 
-export default defineComponent({
+export default {
   setup() {
-    const user = ref('');
-    const password = ref('');
-    return { user, password };
+    const user = '';
+    const password = '';
+    const img0 = ref(null);
+    const img1 = ref(null);
+    const img2 = ref(null);
+    const focus = (event) => {
+      if (event.target.id === 'user') {
+        img1.value.classList.remove('hidden');
+        img0.value.classList.add('hidden');
+        img2.value.classList.add('hidden');
+      }
+      if (event.target.id === 'password') {
+        img2.value.classList.remove('hidden');
+        img0.value.classList.add('hidden');
+        img1.value.classList.add('hidden');
+      }
+    };
+    const blur = () => {
+      if (Array.prototype.includes.call(img0.value.classList, 'hidden')) {
+        img0.value.classList.remove('hidden');
+        img1.value.classList.add('hidden');
+        img2.value.classList.add('hidden');
+      }
+    };
+    const login = () => {
+      if (user === 'root' && password === 'admin') {
+        router.push('/index');
+      } else {
+        console.log('用户名或密码错误!');
+      }
+    };
+    return { user, password, focus, blur, login, img0, img1, img2 };
   }
-});
+};
 </script>
 
 <style lang="scss" scoped>
@@ -46,6 +84,15 @@ export default defineComponent({
       top: -80px;
       width: 120px;
       height: 95px;
+      transform: translate(-50%, 0);
+    }
+
+    .bear-focus {
+      position: absolute;
+      left: 50%;
+      top: -80px;
+      width: 120px;
+      height: 110px;
       transform: translate(-50%, 0);
     }
 
