e10e7dfc52fcbecaf502bbf8deb22433a6b996b3
xiehaopei <15907678645@163.com>
2020-12-10 14:14:39 +0800

    feat:添加404页面

---
+++ admin/src/pages/404.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Author: Haopei Xie
+ * @Date: 2020-12-09 20:55:12
+ * @LastEditTime: 2020-12-09 20:55:44
+ * @LastEditors: Haopei Xie
+ * @Description: 404页面
+ * @FilePath: \We-arts\admin\src\pages\404.vue
+ * @
+-->
+<template>
+  <div class="no">404, 自己返回吧!</div>
+</template>
+
+<style lang="scss" scoped>
+.no {
+  font-size: 30px;
+  padding: 20px;
+}
+</style>
--- admin/src/router.js
+++ admin/src/router.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Haopei Xie
  * @Date: 2020-12-03 22:12:16
- * @LastEditTime: 2020-12-08 16:49:51
+ * @LastEditTime: 2020-12-10 00:00:00
  * @LastEditors: Haopei Xie
  * @Description: 路由管理
  * @FilePath: \We-arts\admin\src\router.js
@@ -18,12 +18,20 @@ const router = createRouter({
     { path: '/', redirect: '/login' },
     {
       path: '/index', component: () => import('./pages/Index.vue'), children: [
+        { path: "/", redirect: '/monitor' },
+        { path: '/monitor', component: () => import('./pages/Monitor.vue') },
         { path: '/article', component: () => import('./pages/article/Article.vue') },
         { path: '/user', component: () => import('./pages/user/user.vue') }
       ]
     },
-    { path: '/login', component: () => import('./pages/Login.vue') }
+    { path: '/login', component: () => import('./pages/Login.vue') },
+    { path: '/404', component: () => import('./pages/404.vue') },
+    { path: '/:pathMatch(.*)*', redirect: '/404' }
   ]
+})
+
+router.beforeEach((to, from) => {
+  console.log(to, from)
 })
 
 export default router;
